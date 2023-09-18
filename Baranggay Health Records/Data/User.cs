342d139aa30fc4f9c1606557334d17b1e193032c
Meedux <herrerojayemark@gmail.com>
2023-09-18 15:24:59 +0800

    Created User Data

---
+++ Baranggay Health Records/Data/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Baranggay_Health_Records.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public String? UserName { get; set; }
+
+        [Required]
+
+        public String? Password { get; set; }
+
+        [Required]
+        public String? FirstName {  get; set; }
+
+        [Required]
+        public String? LastName { get; set; }
+
+        [Required]
+        public String? AccountType { get; set; }
+    }
+}
